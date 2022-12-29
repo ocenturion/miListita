3ca8f7e78e6574a00db671e304315c231e347460
Oscar Centurion <centurion.oscar90@gmail.com>
2022-12-28 21:44:44 -0300

    se agrega manejo de cache

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,15 +1,52 @@
+var cacheName = 'miListitaCache'
+
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/main.js',
+    '/style.css',
+    '/icon.png'
+  ];
+
 // Instala el Service Worker
-self.addEventListener('install', (event) => {
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            console.log('[serviceWorker] Caching app shell')
+            return cache.addAll(filesToCache)
+        })
+    )
     console.log('Service Worker instalado');
   });
   
   // Activa el Service Worker
-  self.addEventListener('activate', (event) => {
+  self.addEventListener('activate', (e) => {
     console.log('Service Worker activado');
   });
   
   // Controla las solicitudes de recursos
-  self.addEventListener('fetch', (event) => {
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        // comprobamos si el recurso solicitado se encuentra en el cache
+        cache.match(e.request).then(function(response){
+            if (response) {
+                console.log('[serviceWorker] Found in cache', e.request.url)
+                return response
+            }
+            // si no esta en cache, lo solicitamos a travez de internet
+            var request = e.request.clone()
+            return fetch(request).then(function(response){
+                // si la solicitud es exitosa, almacenamos el recurso en el cache para futuras solicitudes
+                if (response) {
+                    console.log('[serviceWorker] Caching new resource', e.request.url)
+                    caches.open(cacheName).then(function (cache) {
+                        cache.put(e.request, response)
+                    })
+                }
+                return response
+            })
+        })
+    )
     console.log('Solicitud de recursos recibida');
   });
   
