9ec997c44db2a466de776e39dada7f166b778646
Oscar Centurion <centurion.oscar90@gmail.com>
2022-12-31 01:40:51 -0300

    cambios en el sw

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,4 @@
+/*
 var cacheName = "miListitaCache";
 
 var filesToCache = [
@@ -19,11 +20,6 @@ self.addEventListener("install", (e) => {
   );
   console.log("Service Worker instalado");
 });
-
-// Activa el Service Worker
-// self.addEventListener("activate", (e) => {
-//   console.log("Service Worker activado");
-// });
 
 // Activa el Service Worker y elimina cualquier cache antiguo
 self.addEventListener("activate", (e) => {
@@ -71,4 +67,89 @@ self.addEventListener("fetch", (e) => {
       })
     );
   });
-  
+*/
+
+// Al incrementar el  OFFLINE_VERSION obligará a lanzar el evento de instalación y
+// los caché anteriores serán actualizados desde la red.
+// Esta variable está declarada intencionalmente y no se usa.
+// Agrega un comentario para tu linter si lo deseas:
+// eslint-disable-next-line no-unused-vars
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = "offline";
+// Modifica esto con una diferente URL si es necesario.
+// const OFFLINE_URL = "offline.html";
+const OFFLINE_URL = [
+    "/index.html",
+    "/main.js",
+    "/style.css",
+    "/manifest.json",
+    "/image/icon-144x144.png",
+    "/image/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      // Al definir {cache: 'reload'} en la nueva consulta asegurara que la
+      // respuesta no sea desde el caché de HTTP; i.e., esta será
+      // de la red.
+      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+    })()
+  );
+  // Obliga al service worker que espera a que se convierta en uno activo.
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      // Permite la navegación precargada si tiene compatibilidad
+      // Mira https://developers.google.com/web/updates/2017/02/navigation-preload
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+
+  // Le dice al service worker activo que tome el control inmediato de la página.
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  // Solo queremos llamar al event.respondWith() si es una solicitud de navegación
+  // para una página HTML.
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          // Primero, utiliza una respuesta de precarga de navegación.
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          // Siempre usa la red primero.
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          // El catch solo se dispara cuando se obtiene una excepción
+          // gracias a un error en la red.
+          // Si fetch() regresa una respuesta HTTP valida con un codigo de respuesta en el
+          // rango de 4xx o 5xx, el catch() no se llamará
+          console.log("Fetch failed; returning offline page instead.", error);
+
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })()
+    );
+  }
+
+  // si nuestra condición de if() es falso, el controlador de fetch no atrapará la
+  // solicitud. Si hay más controladores de fetch registrados, ellos tendrán la
+  // oportunidad de llamar a event.respondWith(). De lo contrario, si no hay, no se llamará a
+  // event.respondWith(), la solicitud será controlada por el buscador como si no
+  // los service worker no se hubieran involucrado.
+});
